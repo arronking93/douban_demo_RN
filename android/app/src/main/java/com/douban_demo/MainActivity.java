74aff4be74164f97adfe8707598ef4a5c5e170c2
arronking93 <sword9@126.com>
2019-09-16 14:51:47 +0800

    photo-picker release

--- android/app/src/main/java/com/douban_demo/MainActivity.java
+++ android/app/src/main/java/com/douban_demo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.douban_demo;
 
 import com.facebook.react.ReactActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
+import com.facebook.react.modules.core.PermissionListener; // <- add this import
 
 public class MainActivity extends ReactActivity {
-
+    private PermissionListener listener; // <- add this attribute
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
--- components/tabbars/Me.js
+++ components/tabbars/Me.js
@@ -1,15 +1,47 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+
+import {View, Text, Button, Image} from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+
+var photoOptions = {
+	// 底部弹出框选项
+	title: '请选择',
+	cancelButtonTitle: '取消',
+	takePhotoButtonTitle: '拍照',
+	chooseFromLibraryButtonTitle: '选择相册',
+	quality: 0.75, // 照片的质量
+	allowsEditing: true, // 允许被编辑
+	noData: false, // 不添加日期
+	storageOptions: {
+		skipBackup: true, // ios跳过云端备份
+		path: 'images'
+	}
+};
 
 export default class Me extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			imgURL: ''
+		};
 	}
 	
 	render() {
-		return <View>
-			<Text>这是Me组件</Text>
+		return <View style={{alignItems: 'center', paddingTop: 70}}>
+			<Image source={{uri: this.state.imgURL}} style={{width: 200, height: 200}}></Image>
+			<Button title="拍照" onPress={this.cameraAction}></Button>
 		</View>
+	}
+	
+	cameraAction = () => {
+		ImagePicker.showImagePicker(photoOptions, (response) => {
+			console.log('response' + response);
+			if (response.didCancel) {
+				return
+			}
+			this.setState({
+				imgURL: response.uri
+			});
+		})
 	}
 }
